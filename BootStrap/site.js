e4aa1028e650e708c1f4afee1c9d8853eea7b185
echever5 <echever5@gmail.com>
2015-08-18 17:57:30 -0400

    bootstrap components

---
+++ BootStrap/site.js
@@ -0,0 +1,23 @@
+﻿(function () {
+	"use strict";
+
+	var $pickButton = $("#pickButton");
+
+	$("#reasonDropdown li a").on("click", function () {
+		var reason = $(this).text();
+		$pickButton.text(reason);
+	});
+
+	var $sentDialog = $("#sentDialog");
+
+	$("#contactForm").on("submit",function () {
+		$sentDialog.modal('show');
+		return false;
+	});
+
+	$sentDialog.on("hidden.bs.modal",function () {
+		alert("close");
+	}
+	
+	)
+})();
